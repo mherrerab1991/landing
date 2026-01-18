ecbe5d3af8296c2501da597be9d8636de1d94355
mherrerab1991 <mherrerab1991@gmail.com>
2026-01-18 00:50:52 -0300

    Create _middleware.js

---
+++ functions /_middleware.js
@@ -0,0 +1,39 @@
+// /functions/_middleware.js
+
+function readCookie(request, name) {
+  const c = request.headers.get("Cookie") || "";
+  const m = c.match(new RegExp(`(?:^|; )${name}=([^;]+)`));
+  return m ? m[1] : null;
+}
+
+export async function onRequest(context) {
+  const { request, env, next } = context;
+  const url = new URL(request.url);
+
+  // Deja pasar API, home, login y assets
+  const publicPaths = new Set([
+    "/",
+    "/index.html",
+    "/login.html",
+    "/styles.css",
+     "/ping"
+  ]);
+
+  if (url.pathname.startsWith("/api/")) return next();
+  if (publicPaths.has(url.pathname)) return next();
+  if (url.pathname.startsWith("/favicon")) return next();
+
+  // Protegemos todo lo que esté bajo /app/
+  if (url.pathname.startsWith("/app/")) {
+    const token = readCookie(request, "hb_session");
+    if (!token) return Response.redirect(`${url.origin}/login.html`, 302);
+
+    const sess = await env.HB_AUTH.get(`sess:${token}`);
+    if (!sess) return Response.redirect(`${url.origin}/login.html`, 302);
+
+    return next();
+  }
+
+  // Todo lo demás lo dejamos pasar por ahora
+  return next();
+}
