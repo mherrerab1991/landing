28a4566bf2a1c065de3ac9dbbc598c02adf9879f
mherrerab1991 <mherrerab1991@gmail.com>
2026-02-03 16:39:32 -0300

    Delete functions  directory

--- functions /api/ping.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestGet() {
-  return new Response("pong-" + Date.now(), {
-    status: 200,
-    headers: {
-      "content-type": "text/plain; charset=utf-8",
-      "cache-control": "no-store"
-    }
-  });
-}
